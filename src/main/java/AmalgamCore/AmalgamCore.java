3c81aac03aaf56fe90a24b5bf0698b244d4f7428
mm4cc <mm4ccbox@gmail.com>
2015-01-14 20:31:29 -0500

    initial commit

---
+++ src/main/java/AmalgamCore/AmalgamCore.java
@@ -0,0 +1,20 @@
+package AmalgamCore;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = AmalgamCore.MODID, version = AmalgamCore.VERSION)
+public class AmalgamCore
+{
+    public static final String MODID = "amalgamCore";
+    public static final String VERSION = "0.0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
